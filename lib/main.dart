99a5c4f9d76c8992b9cd8767ba1490378dc21894
toyourheart163 <blive200@gmail.com>
2020-12-14 18:41:18 +0800

    Add Material touch ripples

--- lib/main.dart
+++ lib/main.dart
@@ -5,70 +5,46 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final title = 'InkWell Demo';
+
     return MaterialApp(
-      title: 'Text Field Focus',
-      home: MyCustomForm(),
+      title: title,
+      home: MyHomePage(title: title),
     );
   }
 }
 
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
+class MyHomePage extends StatelessWidget {
+  final String title;
 
-// Define a corresponding State class.
-// This class holds data related to the form.
-class _MyCustomFormState extends State<MyCustomForm> {
-  // Define the focus node. To manage the lifecycle, create the FocusNode in
-  // the initState method, and clean it up in the dispose method.
-  FocusNode myFocusNode;
-
-  @override
-  void initState() {
-    super.initState();
-
-    myFocusNode = FocusNode();
-  }
-
-  @override
-  void dispose() {
-    // Clean up the focus node when the Form is disposed.
-    myFocusNode.dispose();
-
-    super.dispose();
-  }
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text Field Focus'),
+        title: Text(title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // The first text field is focused on as soon as the app starts.
-            TextField(
-              autofocus: true,
-            ),
-            // The second text field is focused on when a user taps the
-            // FloatingActionButton.
-            TextField(
-              focusNode: myFocusNode,
-            ),
-          ],
-        ),
+      body: Center(child: MyButton()),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // The InkWell wraps the custom flat button widget.
+    return InkWell(
+      // When the user taps the button, show a snackbar.
+      onTap: () {
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text('Tap'),
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.all(12.0),
+        child: Text('Flat Button'),
       ),
-      floatingActionButton: FloatingActionButton(
-        // When the button is pressed,
-        // give focus to the text field using myFocusNode.
-        onPressed: () => myFocusNode.requestFocus(),
-        tooltip: 'Focus Second Text Field',
-        child: Icon(Icons.edit),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
