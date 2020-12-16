ce7c40e7a0644da26f6ea43b9789afabb1f1ba8c
toyourheart163 <blive200@gmail.com>
2020-12-16 18:40:10 +0800

    Animate a widget across screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,59 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(HeroApp());
 
-class MyApp extends StatelessWidget {
-  MyApp({Key key}) : super(key: key);
-
+class HeroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Floating App Bar';
-
     return MaterialApp(
-      title: title,
-      home: Scaffold(
-        // No appbar provided to the Scaffold, only a body with a
-        // CustomScrollView.
-        body: CustomScrollView(
-          slivers: <Widget>[
-            // Add the app bar to the CustomScrollView.
-            SliverAppBar(
-              // Provide a standard title.
-              title: Text(title),
-              // Allows the user to reveal the app bar if they begin scrolling
-              // back up the list of items.
-              floating: true,
-              // Display a placeholder widget to visualize the shrinking size.
-              flexibleSpace: Placeholder(),
-              // Make the initial height of the SliverAppBar larger than normal.
-              expandedHeight: 200,
-            ),
-            // Next, create a SliverList
-            SliverList(
-              // Use a delegate to build items as they're scrolled on screen.
-              delegate: SliverChildBuilderDelegate(
-                // The builder function returns a ListTile with a title that
-                // displays the index of the current item.
-                (context, index) => ListTile(title: Text('Item #$index')),
-                // Builds 1000 ListTiles
-                childCount: 1000,
-              ),
-            ),
-          ],
+      title: 'Transition Demo',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        child: Hero(
+          tag: 'imageHero',
+          child: Image.network(
+	    'https://tse1-mm.cn.bing.net/th/id/OET.faa04cc1480c472e9404804041e588d2',
+          ),
         ),
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return DetailScreen();
+          }));
+        },
       ),
     );
   }
 }
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+	      'https://tse1-mm.cn.bing.net/th/id/OET.faa04cc1480c472e9404804041e588d2',
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
+
