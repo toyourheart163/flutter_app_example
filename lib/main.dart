64a24855e9fc2a01354d01bee138017c857928dd
toyourheart163 <blive200@gmail.com>
2020-12-16 13:38:35 +0800

    Fade in images with a placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var title = 'Web Images';
+    final title = 'Fade in images';
 
     return MaterialApp(
       title: title,
@@ -13,8 +16,20 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: Image.network('https://github.com/flutter/plugins/raw/master/packages/video_player/video_player/doc/demo_ipod.gif?raw=true'),
+        body: Stack(
+          children: <Widget>[
+            Center(child: CircularProgressIndicator()),
+            Center(
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                // image: 'https://picsum.photos/250?image=9',
+		image: 'https://tse1-mm.cn.bing.net/th/id/OET.faa04cc1480c472e9404804041e588d2',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
