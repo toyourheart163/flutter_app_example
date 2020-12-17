2543c541c56db79940cfa7c5cfef02ffb0e82626
toyourheart163 <blive200@gmail.com>
2020-12-17 16:48:34 +0800

    Fetch data from the internet

--- lib/main.dart
+++ lib/main.dart
@@ -9,109 +9,69 @@ Future<Album> fetchAlbum() async {
       await http.get('https://jsonplaceholder.typicode.com/albums/1');
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response, then parse the JSON.
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
     return Album.fromJson(jsonDecode(response.body));
   } else {
-    // If the server did not return a 200 OK response, then throw an exception.
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
 
-Future<Album> deleteAlbum(String id) async {
-  final http.Response response = await http.delete(
-    'https://jsonplaceholder.typicode.com/albums/$id',
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-  );
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON. After deleting,
-    // you'll get an empty JSON `{}` response.
-    // Don't return `null` otherwise `snapshot.hasData`
-    // will always return false on `FutureBuilder`.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a "200 OK response",
-    // then throw an exception.
-    throw Exception('Failed to delete album.');
-  }
-}
-
 class Album {
+  final int userId;
   final int id;
   final String title;
 
-  Album({this.id, this.title});
+  Album({this.userId, this.id, this.title});
 
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
+      userId: json['userId'],
       id: json['id'],
       title: json['title'],
     );
   }
 }
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   MyApp({Key key}) : super(key: key);
 
   @override
-  _MyAppState createState() {
-    return _MyAppState();
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<Album> _futureAlbum;
+  Future<Album> futureAlbum;
 
   @override
   void initState() {
     super.initState();
-    _futureAlbum = fetchAlbum();
+    futureAlbum = fetchAlbum();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Delete Data Example',
+      title: 'Fetch Data Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Delete Data Example'),
+          title: Text('Fetch Data Example'),
         ),
         body: Center(
           child: FutureBuilder<Album>(
-            future: _futureAlbum,
+            future: futureAlbum,
             builder: (context, snapshot) {
-              // If the connection is done,
-              // check for response data or an error.
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('${snapshot.data?.title ?? 'Deleted'}'),
-                      ElevatedButton(
-                        child: Text('Delete Data'),
-                        onPressed: () {
-                          setState(() {
-                            _futureAlbum =
-                                deleteAlbum(snapshot.data.id.toString());
-                          });
-                        },
-                      ),
-                    ],
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
               }
 
               // By default, show a loading spinner.
